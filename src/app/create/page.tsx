6c9a744686dfd0ec8da3487d3381125e886d9cca
Faizan Bardai <faizan.badruddin.bardai@gmail.com>
2025-08-21 21:20:22 +0200

    feat: add Create page with form handling using react-hook-form; update package dependencies

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,27 @@
+'use client'
+
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type Inputs = {
+  title: string
+  date: string
+  time: string
+}
+
 const CreatePage = () => {
+  const { register, handleSubmit } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
     <div>
       <h1>Create a New Entry</h1>
-      {/* Add your form or content here */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('title')} placeholder="Title" />
+        <input {...register('date')} type="date" />
+        <input {...register('time')} type="time" />
+        <button type="submit">Create</button>
+      </form>
     </div>
   )
 }
